243ff0cca46c147e958cac6d1a6131fd9e89c8a0
Willyan <willy4nnp@gmail.com>
2024-10-11 00:17:29 -0300

    frontend/feature: converte checkbox

--- Frontend/src/components/Checkbox/index.tsx
+++ Frontend/src/components/Checkbox/index.tsx
@@ -1,24 +1,68 @@
-import "./checkbox.css";
+import styled from "styled-components";
 import { useState } from "react";
 
+const CheckboxStyled = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	width: fit-content;
+
+	-webkit-appearance: none;
+	appearance: none;
+	cursor: pointer;
+	opacity: 50%;
+	outline: none;
+
+	.label {
+		color: #0a0a15;
+		width: fit-content;
+		cursor: pointer;
+		user-select: none;
+	}
+
+	.container-checkbox {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 6px;
+	}
+
+	&.checked {
+		width: 12px;
+		height: 12px;
+		border: 1px solid #0a0a15;
+		cursor: pointer;
+		outline: none;
+	}
+
+	&.checked:checked {
+		background-color: #646cff;
+	}
+`;
+
 interface CheckboxProps {
-  label: string;
-  checked?: boolean;
-  onChange: () => void;
+	label: string;
+	checked?: boolean;
+	onChange: () => void;
 }
 
 export const Checkbox = ({ label, checked, onChange }: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState(false);
-  return (
-    <div className="container-checkbox">
-      <input
-        type="checkbox"
-        checked={checked ? checked : isChecked}
-        onChange={onChange}
-        onClick={() => (isChecked ? setIsChecked(false) : setIsChecked(true))}
-        className={isChecked ? "checked" : "box"}
-      />
-      <label className="label">{label}</label>
-    </div>
-  );
+	const [isChecked, setIsChecked] = useState(false);
+	return (
+		<CheckboxStyled className="container-checkbox">
+			<input
+				id="myCheckbox"
+				type="checkbox"
+				checked={checked ? checked : isChecked}
+				onChange={onChange}
+				onClick={() =>
+					isChecked ? setIsChecked(false) : setIsChecked(true)
+				}
+				className={isChecked ? "checked" : "box"}
+			/>
+			<label htmlFor="myCheckbox" className="label">
+				{label}
+			</label>
+		</CheckboxStyled>
+	);
 };
