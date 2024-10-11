367108b8ffe4c0c448aa3b4accfe8728c36531c5
Willyan <willy4nnp@gmail.com>
2024-10-10 22:21:14 -0300

    feature: converte button styled and add label

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,33 +5,40 @@ import { Checkbox } from "./components/Checkbox";
 import { Card } from "./components/Card";
 
 function App() {
-
-  return (
-    <div className="container-app">
-      <Button onClick={() => alert("Hello, world!")}>Click me!</Button>
-      <Input
-        readOnly={false}
-        type="email"
-        placeholder="Insira seu email"
-        icon="mail"
-        color="#0a0a1579"
-        size={16}
-      />
-      <Input
-        readOnly={false}
-        type="password"
-        placeholder="Insira sua senha"
-        icon="mail"
-        color="#0a0a1579"
-        size={16}
-      />
-      <Checkbox
-        label="Aceita os termos e condições?"
-        onChange={() => console.log("opa")}
-      ></Checkbox>
-      <Card data="10/10/2024 - 17h00" status="Pendente" subject="Reunião importante com o RH"></Card>
-    </div>
-  );
+	return (
+		<div className="container-app">
+			<Button
+				onClick={() => alert("Hello, world!")}
+				width="full"
+				label="Click Me"
+			/>
+			<Input
+				readOnly={false}
+				type="email"
+				placeholder="Insira seu email"
+				icon="mail"
+				color="#0a0a1579"
+				size={16}
+			/>
+			<Input
+				readOnly={false}
+				type="password"
+				placeholder="Insira sua senha"
+				icon="mail"
+				color="#0a0a1579"
+				size={16}
+			/>
+			<Checkbox
+				label="Aceita os termos e condições?"
+				onChange={() => console.log("opa")}
+			></Checkbox>
+			<Card
+				data="10/10/2024 - 17h00"
+				status="Pendente"
+				subject="Reunião importante com o RH"
+			></Card>
+		</div>
+	);
 }
 
 export default App;
