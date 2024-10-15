b8b0fd38380a54c4471ec47a6b0c07661a5bfa85
Renato Barbosa <tenat.renato@gmail.com>
2024-10-15 17:45:43 -0300

    refactor: refactor get messages route

--- Backend/src/controller/dialogController.ts
+++ Backend/src/controller/dialogController.ts
@@ -4,10 +4,19 @@ import errorResponse from '../utils/errorResponse';
 
 export const dialogController = {
   getMessages: async (req: Request, res: Response): Promise<void> => {
-    const dialog_id = req.params.dialog_id;
+    const scheduleId = req.params.scheduleId;
     const user = req.user!;
     try {
-      const messages = await dialogServices.getMessages(dialog_id, user.id);
+      if (!scheduleId) {
+        errorResponse(res, {
+          error: 'BAD_REQUEST',
+          message: 'Por favor, informe o ID do agendamento.',
+          statusCode: 400,
+        });
+        return;
+      }
+
+      const messages = await dialogServices.getMessages(scheduleId, user.id);
 
       res.status(200).json(messages);
     } catch (error: any) {
