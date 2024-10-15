9aa42f6d56018a574e8ecf129c71de8bfb897c1a
Leonardo <leomoriva@gmail.com>
2024-10-14 21:52:41 -0300

    Added Registration call page and made some fix in snackbar.

--- Frontend/src/types/schedules.d.ts
+++ Frontend/src/types/schedules.d.ts
@@ -6,10 +6,10 @@ interface Invite {
   interface Schedule {
     schedule_id: number;
     status: 'pending' | 'confirmed' | 'cancelled';
-    proposed_date?: Date; // Opcional, apenas quando o status for 'pending'
-    time_limit?: Date; // Opcional, apenas quando o status for 'pending'
-    event_date?: Date; // Opcional, apenas quando o status for 'confirmed'
-    event_time?: string; // Opcional, apenas quando o status for 'confirmed'
+    proposed_date?: Date;
+    time_limit?: Date;
+    event_date?: Date;
+    event_time?: string;
     event_title: string;
     event_description: string;
     is_host: boolean;
