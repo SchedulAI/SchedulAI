76c9c32be99e59b14d8a196e7a8f57b9bc7e0844
Renato Barbosa <tenat.renato@gmail.com>
2024-10-17 16:24:51 -0300

    feat: implement createInvites tool

--- Backend/src/tools/tools.ts
+++ Backend/src/tools/tools.ts
@@ -1,17 +1,20 @@
 import updateScheduleInfo from './updateScheduleInfo';
 import createInvitedEmails from './createInvitedEmails';
 import createAvailabilities from './createAvailabilities';
+import createInvites from './createInvites';
 
 const tools = [
   updateScheduleInfo,
   createInvitedEmails,
   createAvailabilities,
+  createInvites,
 ];
 
 export const toolsByName: { [key: string]: any } = {
   updateScheduleInfo: updateScheduleInfo,
   createInvitedEmails: createInvitedEmails,
   createAvailabilities: createAvailabilities,
+  createInvites: createInvites,
 };
 
 export default tools;
