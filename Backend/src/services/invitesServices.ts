0d39756c52cf1688554d8fd0af8ff59033ed0614
Renato Barbosa <tenat.renato@gmail.com>
2024-10-18 12:49:05 -0300

    feat: implement a route to create a single invite

--- Backend/src/services/invitesServices.ts
+++ Backend/src/services/invitesServices.ts
@@ -1,11 +1,20 @@
 import { Invites } from '../entities/invitesEntity';
+import { availabilityRepository } from '../repository/availabilityRepository';
+import { dialogRepository } from '../repository/dialogRepository';
+import { invitedEmailsRepository } from '../repository/invitedEmailsRepository';
 import { invitesRepository } from '../repository/invitesRepository';
 import { scheduleRepository } from '../repository/scheduleRepository';
 import { userRepository } from '../repository/userRepository';
-import { ConflictException, NotFoundException } from '../utils/exceptions';
+import {
+  ConflictException,
+  ForbiddenException,
+  NotFoundException,
+} from '../utils/exceptions';
+import formatAvailability from '../utils/formatAvailability';
+import { dialogServices } from './dialogServices';
 
 export const invitesServices = {
-  createInvites: async (
+  createInvitesByListOfEmails: async (
     scheduleId: string,
     userEmails: string[]
   ): Promise<Invites[]> => {
@@ -94,5 +103,61 @@ export const invitesServices = {
 
     const updatedInvite = await invitesRepository.updateStatus(foundUser.id);
     return updatedInvite;
+  },
+
+  createInvite: async (
+    userId: string,
+    scheduleId: string
+  ): Promise<Invites> => {
+    const schedule = await scheduleRepository.getScheduleById(scheduleId);
+    if (!schedule) {
+      throw new NotFoundException('Agendamento não encontrado');
+    }
+
+    const user = await userRepository.findById(userId);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const invitedEmails =
+      await invitedEmailsRepository.getInvitedEmailsByScheduleId(scheduleId);
+
+    const isInvited = invitedEmails.some(
+      (invitedEmail) => invitedEmail.email === user.email
+    );
+
+    if (!isInvited) {
+      throw new ForbiddenException(
+        'O usuário não está convidado para o agendamento.'
+      );
+    }
+    
+    const createdInvite = await invitesRepository.createInvite(
+      scheduleId,
+      user.id
+    );
+
+    const hostAvailabilities =
+      await availabilityRepository.getAvailabilitiesByUserAndSchedule(
+        schedule.user_id,
+        scheduleId
+      );
+
+    const formatedAvailabilities = hostAvailabilities
+      .map((availability) => formatAvailability(availability))
+      .join('\n');
+
+    const dialog = await dialogServices.createDialog(
+      scheduleId,
+      createdInvite.user_id,
+      createdInvite.id
+    );
+
+    const message = `Olá! Você foi convidado para o agendamento "${schedule.title}", criado pelo "${schedule.host_name}", por favor, me informe em se tem disponibilide em uma ou mais das seguintes disponibilidades:\n\n${formatedAvailabilities}
+      `;
+
+    await dialogRepository.saveMessage(dialog.id, message, 'IA');
+    
+    return createdInvite;
   },
 };
