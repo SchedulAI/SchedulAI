43b53f8ecf7e28bf63ec690828cfab60784a2843
Leonardo <leomoriva@gmail.com>
2024-10-23 01:43:06 -0300

    Enhanced both utils

--- Frontend/src/Utils/FormatTime.ts
+++ Frontend/src/Utils/FormatTime.ts
@@ -1,7 +1,11 @@
 export const FormatTime = (time: number) => {
-  const formatedTime = time / 60;
+  const formatedTime = Math.floor(time / 60);
   const remainingMinutes = time % 60;
   const formattedMinutes =
     remainingMinutes === 0 ? '00' : remainingMinutes.toString();
+
+  if (formatedTime === 0) {
+    return `${formattedMinutes}min`;
+  }
   return `${formatedTime}h${formattedMinutes}`;
 };
